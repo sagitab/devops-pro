1d9fb7b46145264677340b983bc2d84e65f977c0
sagitab <60473098+sagitab@users.noreply.github.com>
2025-08-30 17:00:53 +0300

    try again with appspec

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,8 @@
+import json
+
 def lambda_handler(event, context):
+    # TODO implement
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda! Pipeline working!'
+        'body': json.dumps('Hello from Lambda!')
     }
